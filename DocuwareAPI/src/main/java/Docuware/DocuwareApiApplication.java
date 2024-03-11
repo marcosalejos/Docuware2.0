68a14faabe919913b143ef01cfaa536d29cab183
Marcos Alejos <marcosalejosbermell@gmail.com>
2024-03-11 10:39:14 +0100

    Creación del backend con Spring

---
+++ DocuwareAPI/src/main/java/Docuware/DocuwareApiApplication.java
@@ -0,0 +1,13 @@
+package Docuware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuwareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuwareApiApplication.class, args);
+	}
+
+}
---
+++ DocuwareAPI/src/test/java/Docuware/DocuwareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Docuware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuwareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
